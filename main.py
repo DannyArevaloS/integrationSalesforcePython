f6605f19ad763d4daa1219af27c8153a4c2e61d1
DannyArevaloS <darevalo_dev@outlook.es>
2024-09-29 15:47:37 +0200

    commit 2

--- main.py
+++ main.py
@@ -9,37 +9,16 @@ username = config('USERNAME_SF')
 password = config('PASSWORD_SF')
 token = config('TOKEN_SF')
 
-sf = Salesforce(username=username,password=password,security_token=token)
-print(sf)
+sf =Salesforce(username=username, password=password, security_token=token)
+res = sf.query("SELECT Id, Name, Type FROM Account WHERE Type = 'Suppliers'")
 
-response = sf.query("SELECT Id, Name, Type, Zone__c, Province__c "
-                    "FROM Account "
-                    "WHERE Zone__c = 'Centro'")
-#print(response)
-#print(response['records'])
-#print(response['totalSize'])
+for i in res['records']:
+    print(f"ID: {i['Id']}, TYPE: {i['Type']}, NAME: {i['Name']}")
 
-#for i in response['records']:
-#    print(i)
-
-for i in response['records']:
-    print(f"ID: {i['Id']}, Name: {i['Name']}")
-
-df = pd.DataFrame(response['records'])
+df = pd.DataFrame(res['records'])
 pd.set_option('display.max_columns', 100)
 pd.set_option('expand_frame_repr', True)
-#print(df)
 
-fileInfo = {
-    "Name":"Prueba desde Python version 3",
-    "Type": "Market",
-    "Zone__c" : "Centro",
-    "Province__c" : "Zaragoza"
-}
-
-acct : sf.Account.create(fileInfo)
-
-current_timestamp = datetime.now()
-df.to_csv("{0}_sfdc_data.csv".format(current_timestamp.strftime("%Y-%m-%d")), index=False)
+current_timestamp =datetime.now()
 
 
